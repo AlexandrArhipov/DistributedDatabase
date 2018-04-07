16e8b824cbb9a6e890c86f0612c5ac03e68657ad
Alexandr Arhipov <arhip.sasha.26@yandex.ru>
2018-04-07 09:12:54 +0300

    Begin html

---
+++ DistributedDatabase/Controllers/FormController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DistributedDatabase.Models;
+using System;
+
+namespace DistributedDatabase.Controllers
+{
+    public class FormController : Controller
+    {
+        [HttpGet]
+        public IActionResult UserForm() => View();
+        
+        [HttpGet]
+        public IActionResult CharacterForm() => View();
+        
+        [HttpPost]
+        public IActionResult UserForm(string login, string password, string email, string dateOfBirth, string country)
+        {
+            string authData = $"Login: {login}   Password: {password} Email: {email}  DateOfBirth : {dateOfBirth} Country: {country}";
+            return Content(authData);
+        }
+        
+        [HttpPost]
+        public IActionResult CharacterForm(string name, string sex, string skin, string height, string weight, string @class, string race)
+        {
+            return Content("Name: " + name);
+        }
+
+        public IActionResult Error() => 
+            View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
